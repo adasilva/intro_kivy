f09c9633d0b414a9b96df91682bbb47b34966929
adasilva <awesomeashley527@gmail.com>
2014-09-20 13:27:08 -0500

    finished 01_buildingblocks, including slides in main presentation

--- 01_buildingblocks/workoutApp.py
+++ 01_buildingblocks/workoutApp.py
@@ -6,20 +6,20 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 
 
-class UserInfoScreen(GridLayout):
+class MainScreen(GridLayout):
     def __init__(self,**kwargs):
-        super(UserInfoScreen,self).__init__(**kwargs) # implements the features of a GridLayout
+        super(MainScreen,self).__init__(**kwargs) # implements the features of a GridLayout (the base class of MinScreen)
         self.cols=3
         
         l1=Label(text='Workout')
         l2=Label(text='Day')
         l3=Label(text='Time')
 
-        in1=TextInput(multiline=False)
+        in1=TextInput(multiline=False)  # multiline determines if more than one line of text is allowed
         in2=TextInput(multiline=False)
         in3=TextInput(multiline=False)
 
-        self.add_widget(l1)
+        self.add_widget(l1)  # Adds widgets to the layout
         self.add_widget(l2)
         self.add_widget(l3)
         self.add_widget(in1)
@@ -27,13 +27,13 @@ class UserInfoScreen(GridLayout):
         self.add_widget(in3)
         
 
-class TodoApp(App):
+class WorkoutApp(App):
     def build(self):
-        return UserInfoScreen()
+        return MainScreen()
 
 
 if __name__=='__main__':
-    TodoApp().run()
+    WorkoutApp().run()
 
 
 
