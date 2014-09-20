3bfc412dd0d120bd3917f72af1095da84430b501
adasilva <awesomeashley527@gmail.com>
2014-09-20 14:14:42 -0500

    added 02_kivylanguage and associated slides

---
+++ 02_kivylanguage/workoutApp.py
@@ -0,0 +1,24 @@
+from kivy.app import App
+from kivy.app import Widget
+
+from kivy.uix.gridlayout import GridLayout
+
+
+class MainScreen(GridLayout):
+    def __init__(self,**kwargs):
+        super(MainScreen,self).__init__(**kwargs) # implements the features of a GridLayout (the base class of MinScreen)
+        
+
+class WorkoutApp(App):
+    def build(self):
+        return MainScreen()
+
+
+if __name__=='__main__':
+    WorkoutApp().run()
+
+
+
+# NOTE: running is diff on diff platforms
+# python main.py
+# kivy main.py
