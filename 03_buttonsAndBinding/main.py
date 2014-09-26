351b138605dc38a5b315cf943d0383d7c74c674b
adasilva <awesomeashley527@gmail.com>
2014-09-26 16:17:29 -0500

    added checkboxes

--- 03_buttonsAndBinding/main.py
+++ 03_buttonsAndBinding/main.py
@@ -11,21 +11,24 @@ class MainScreen(GridLayout):
     def save(self):
         '''Saves the data from the input to a text file.
     It is bound to the save button'''
-        workout = self.ids.workoutInput.text
-        day = self.ids.dayInput.text
-        time = self.ids.timeInput.text
+        status1 = self.ids.checkbox1.active
+        workout1 = self.ids.workoutInput1.text
+        day1 = self.ids.dayInput1.text
+        time1 = self.ids.timeInput1.text
 
+        status2 = self.ids.checkbox2.active
         workout2 = self.ids.workoutInput2.text
         day2 = self.ids.dayInput2.text
         time2 = self.ids.timeInput2.text
 
+        status3 = self.ids.checkbox3.active
         workout3 = self.ids.workoutInput3.text
         day3 = self.ids.dayInput3.text
         time3 = self.ids.timeInput3.text
         with open('workoutData.txt','a') as f:
-            f.write('%s, %s, %s\n' %(workout, day, time))
-            f.write('%s, %s, %s\n' %(workout2, day2, time2))
-            f.write('%s, %s, %s\n' %(workout3, day3, time3))
+            f.write('%s, %s, %s, %s\n' %(status1, workout1, day1, time1))
+            f.write('%s, %s, %s, %s\n' %(status2, workout2, day2, time2))
+            f.write('%s, %s, %s, %s\n' %(status3, workout3, day3, time3))
 
         return None
         
